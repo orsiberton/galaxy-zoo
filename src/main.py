c7fbc284f483fb1e250d57b46fa8d36adf070b53
Bruno Berton <bberton@ciandt.com>
2018-06-09 12:47:08 -0300

    add project structure

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+
+# Main será usado para orquestrar os outros scripts python
+# Executar o pré-processamento nas imagens
+# TALVEZ, fazer o data augmentation
+# rodar o treino
+# rodar o test
+# criar o csv final com as respostas
