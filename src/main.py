ad1af636ad674a8e15d7fdabdf1a34f0f07baf4f
Bruno Berton <bberton@ciandt.com>
2018-06-09 22:18:00 -0300

    add pre process script to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-
-
 # Main será usado para orquestrar os outros scripts python
 # Executar o pré-processamento nas imagens
 # TALVEZ, fazer o data augmentation
 # rodar o treino
 # rodar o test
 # criar o csv final com as respostas
+
+import preProcessImage
+
+
+def main():
+    preProcessImage.main()
+
+
+if __name__ == '__main__':
+    main()
