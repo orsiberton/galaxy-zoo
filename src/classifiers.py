a77ecc4056774989bc98edb92fdb0e552aa923d7
Bruno Berton <bberton@ciandt.com>
2018-06-12 16:15:37 -0300

    add usefull links

---
+++ src/classifiers.py
@@ -0,0 +1,8 @@
+"""
+
+http://scikit-learn.org/stable/modules/ensemble.html#forest
+http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+http://scikit-learn.org/stable/modules/multiclass.html#multioutput-regression
+
+
+"""
